edeb795e24b68cbea0bab3410b9a220b1ba2e317
Corey Ladovsky <coreyskymusic@gmail.com>
2020-02-17 23:13:20 -0500

    commas

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,8 +2,9 @@ import React from 'react';
 import "../css/Display.css";
 
 const Display = ({num}) => {
+    const  numberWithCommas = (x) =>  x.replace(/\B(?=(\d{3})+(?!\d))/g, ","); // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
     return(
-        <div className={"display"}>{num}</div>
+        <div className={"display"}>{numberWithCommas(num)}</div>
     )
 }
 
