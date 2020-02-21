63e507e14d72d8e0ea31b7f3ea63882c703e02a5
Corey Ladovsky <coreyskymusic@gmail.com>
2020-02-20 23:43:52 -0500

    Updates

---
+++ precache-manifest.2d9e1c97a0192cc920a17173fa1c055f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f009422a42b1cba1ffebffa0e491bd02",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "57361a935c16a7398319",
+    "url": "/calculator/static/css/main.7ee8759a.chunk.css"
+  },
+  {
+    "revision": "ff8a96c79d7a6bac4235",
+    "url": "/calculator/static/js/2.c6b39071.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.c6b39071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57361a935c16a7398319",
+    "url": "/calculator/static/js/main.087d3b7e.chunk.js"
+  },
+  {
+    "revision": "e7c66829e18887417eff",
+    "url": "/calculator/static/js/runtime-main.afeb6a86.js"
+  }
+]);
